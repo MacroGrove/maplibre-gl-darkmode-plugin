13d97776bbd05ec7f4b6d344b79df46556649a67
MacroGrove <71152799+MacroGrove@users.noreply.github.com>
2022-05-27 22:22:13 -0400

    functions in index.html

--- static/scripts/maplibre-gl-darkmode-plugin.js
+++ static/scripts/maplibre-gl-darkmode-plugin.js
@@ -1,68 +1,28 @@
 class MapLibreDarkMode {
-
-    constructor(options) {
-
-        options = Object.assign({}, options);
-
-        if (!(this instanceof MapLibreDarkMode)) {
-            throw new Error('Instantiation Error: MapLibreDarkMode needs to be called with the new keyword.');
-        }
-
-        // Booleans
-        this.isLight = options.isLight || true;
-
-        // Methods
-        this.addButton = this.addButton.bind(this);
-        this.changeMode = this.changeMode.bind(this);
-    }
-
     /**
-     * 
-     */
-    changeMode() {
-        console.log("change!");
-        let style = this._map.getStyle();
-        let button = document.getElementsByClassName("maplubregl-ctrl");
-
-        if (this.isLight) {
-            button.innerHTML = "light mode";
-            style = "https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json"
-            this.isLight = false;
-        } else {
-            button.innerHTML = "light mode";
-            style = "https://tiles.stadiamaps.com/styles/alidade_smooth.json"
-            this.isLight = true;
-        }
-
-        this._map.setStyle(Object.assign({}, style));
-    }
-
-    // initialize() {
-
-    // }
-
-    addButton() {
-        let button = document.createElement('button');
-        button.innerHTML = "dark mode";
-        button.className = "maplubregl-ctrl";
-        button.onclick = function () {
-            this.changeMode();
-        };
-
-        return button;
-    }
-
-    /**
-     * 
-     * @param {*} map 
-     */
+      * 
+      * @param {*} map 
+      */
     onAdd(map) {
         this._map = map;
+        this._map.on('load', function () {
+            document.getElementById('mode')
+                .addEventListener('click', function (event) {
+                    const style = event.target.value;
+                    let isLight = true;
+                    console.log("clicks");
+                    if (style === "alidade_smooth") {
+                        isLight = false;
+                    }
 
-        this._button = this.addButton();
+                    this._map.setStyle(`https://tiles.stadiamaps.com/styles/${isLight ? 'alidade_smooth_dark' : 'alidade_smooth'}}.json`)
+                });
+        });
+
+        // this._button = this.addButton();
         // this._button.addEventListener('click', this.changeMode);
 
-        return this._button;
+        // return this._button;
     }
 
     /**
